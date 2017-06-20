9a33364dc07a7d9b5948db9a3840675a4f063576
jorge.gomez <jorge.gomez@TEKSJONA07>
2017-06-20 16:37:32 -0600

    Release 0.2 chage login

--- App1/App1/obj/Debug/android/src/app1/app1/R.java
+++ App1/App1/obj/Debug/android/src/app1/app1/R.java
@@ -17,14 +17,19 @@ public final class R {
         public static final int logo=0x7f020003;
     }
     public static final class id {
-        public static final int button1=0x7f050003;
-        public static final int checkedTextView1=0x7f050004;
-        public static final int editText1=0x7f050001;
-        public static final int editText2=0x7f050002;
-        public static final int imageView1=0x7f050000;
+        public static final int button1=0x7f050007;
+        public static final int checkedTextView1=0x7f050008;
+        public static final int editText1=0x7f050005;
+        public static final int editText2=0x7f050006;
+        public static final int imageView1=0x7f050004;
+        public static final int linearLayout1=0x7f050000;
+        public static final int space1=0x7f050001;
+        public static final int space2=0x7f050002;
+        public static final int view1=0x7f050003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int home=0x7f030000;
+        public static final int login=0x7f030001;
     }
     public static final class string {
         public static final int ApplicationName=0x7f040001;
