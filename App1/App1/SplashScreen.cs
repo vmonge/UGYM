2dfbf565743417c53588ea84ae2440f98590cbc4
jorge.gomez <jorge.gomez@TEKSJONA07>
2017-06-27 10:00:40 -0600

    Release 0.3 Bug version of SDK

---
+++ App1/App1/SplashScreen.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace UGYM
+{
+    [Activity(Theme ="@style/MyTheme.Splash",MainLuncher = true,NoHistory =true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task starWork = new Task(() =>
+            {
+                Task.Delay(3000);
+            });
+
+            startupWork.ContinueWith(t => {
+                StartActivity(new Intent(Application.Context, typeof(HomeActivity)));
+            },TaskSheduler.FromCurrentSynchronizationContext());
+
+            starWork.Start();
+        }
+    }
+}
+
