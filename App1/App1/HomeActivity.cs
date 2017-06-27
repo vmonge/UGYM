4d2d1fae8173af803adf938cb9b6e590373e4768
jorge.gomez <jorge.gomez@TEKSJONA07>
2017-06-27 15:32:01 -0600

    Release 0.3 bug "NoSuchFieldError: No static field listView1 of type I in class Lcom/disdemo/R$id; or its superclasses"

--- App1/App1/HomeActivity.cs
+++ App1/App1/HomeActivity.cs
@@ -10,20 +10,18 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Support.V7.App;
-using GR.Net.Maroulis.Library;
+using App1;
 
 namespace UGYM
 {
     [Activity(Label = "HomeActivity", Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
     public class HomeActivity : AppCompatActivity
     {
-        int count = 0;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            SetContentView(Resource.Layout.Home);
+            SetContentView(Resource.Layout.Login);
         }
     }
 }
---
+++ App1/App1/SplashActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using GR.Net.Maroulis.Library;
+using Android.Graphics;
+using Android.Views;
+using App1;
+
+namespace UGYM
+{
+    [Activity(Label = "SplashActivity",MainLauncher = true, Icon = "@drawable/logo_nuevo" , Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class SplashActivity : AppCompatActivity
+    {
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            EasySplashScreen config = new EasySplashScreen(this)
+                .WithFullScreen()
+                .WithTargetActivity(Java.Lang.Class.FromType(typeof(HomeActivity)))
+                .WithSplashTimeOut(5000)
+                .WithBackgroundResource(Color.White)
+                .WithHeaderText("Header")
+                .WithFooterText("Copyright 2016")
+                .WithBeforeLogoText("My cool company")
+                .WithLogo(Resource.Drawable.logo_nuevo)
+                .WithAfterLogoText("Some more details");
+
+                config.BeforeLogoTextView.SetTextColor(Color.Gray);
+                config.FooterTextView.SetTextColor(Color.Gray);
+                config.AfterLogoTextView.SetTextColor(Color.Gray);
+
+            View view = config.Create();
+
+            //SetContentView(Resource.Layout.Splash);
+
+        }
+    }
+}
+
